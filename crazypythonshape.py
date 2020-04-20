c18de4fc722bc8188cf0b82b49cb4866e2409b7a
thedizzyprogrammer <akprogramming@outlook.com>
2020-04-20 14:14:57 +0100

    Add files via upload

---
+++ crazypythonshape.py
@@ -0,0 +1,19 @@
+import turtle
+
+
+turtle.bgcolor("black")
+turtle.speed(9*10^800)
+turtle.color("pink")
+
+side=80
+turtle.penup()
+turtle.goto(0,0) 
+turtle.pendown()
+
+for side in range (1,400):
+  turtle.forward(side)
+  turtle.left(92)
+  side=side+7
+
+turtle.penup()
+turtle.goto(500,500)
